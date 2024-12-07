46386ad734adf5ed708ef895f41356d170f332ab
Falcon-slayer123 <himanshutrivedi133@gmail.com>
2024-12-07 21:34:12 +0530

    Initial Commit

---
+++ src/test/java/Java_Spring_Boot_CRUD/CRUD/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package Java_Spring_Boot_CRUD.CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
